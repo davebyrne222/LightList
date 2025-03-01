0470353e7dc92c83d320e8e2a55f7e823036efdf
davidbyrne <david.byrne222@gmail.com>
2025-03-01 16:21:09 +0000

    Fixed bug in navigating to root page causing crash

--- LightList/ViewModels/NavBarViewModel.cs
+++ LightList/ViewModels/NavBarViewModel.cs
@@ -22,31 +22,43 @@ public class NavBarViewModel
 
     private static void OpenMenu()
     {
-        Debug.WriteLine("Opening Flyout");
+        Debug.WriteLine("---[NavBarViewModel.OpenMenu] Opening Flyout");
         Shell.Current.FlyoutIsPresented = true;
     }
 
     private async void AddTask()
     {
+        if (Shell.Current.CurrentPage is Views.TaskPage)
+            return;
+        
         Debug.WriteLine($"---[NavBarViewModel.AddTask] Navigating to {nameof(Views.TaskPage)}");
         await Shell.Current.GoToAsync(nameof(Views.TaskPage));
     }
     
     private async void ViewAllTasks()
     {
+        if (Shell.Current.CurrentPage is Views.AllTasksPage)
+            return;
+        
         Debug.WriteLine($"---[NavBarViewModel.ViewAllTasks] Navigating to {nameof(Views.AllTasksPage)}");
-        await Shell.Current.GoToAsync(nameof(Views.AllTasksPage));
+        await Shell.Current.GoToAsync($"//{nameof(Views.AllTasksPage)}", true);
     }
     
     private async void ViewTasksByDueDate()
     {
+        if (Shell.Current.CurrentPage is Views.TasksByDueDatePage)
+            return;
+        
         Debug.WriteLine($"---[NavBarViewModel.ViewTasksByDueDate] Navigating to {nameof(Views.TasksByDueDatePage)}");
-        await Shell.Current.GoToAsync(nameof(Views.TasksByDueDatePage));
+        await Shell.Current.GoToAsync($"//{nameof(Views.TasksByDueDatePage)}", true);
     }
     
     private async void ViewTasksByLabel()
     {
+        if (Shell.Current.CurrentPage is Views.TasksByLabelPage)
+            return;
+        
         Debug.WriteLine($"---[NavBarViewModel.ViewTasksByLabel] Navigating to {nameof(Views.TasksByLabelPage)}");
-        await Shell.Current.GoToAsync(nameof(Views.TasksByLabelPage));
+        await Shell.Current.GoToAsync($"//{nameof(Views.TasksByLabelPage)}", true);
     }
 }
