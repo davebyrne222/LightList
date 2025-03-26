9c830a57ed88e426612d570a159c13dc2a3cf76d
davidbyrne <david.byrne222@gmail.com>
2025-03-26 22:44:59 +0000

    Added default locale and changed date picker to calendar type
    # Conflicts:
    #	LightList/App.xaml.cs
    #	LightList/Platforms/iOS/Info.plist

--- LightList/App.xaml.cs
+++ LightList/App.xaml.cs
@@ -1,6 +1,10 @@
-﻿using LightList.Data;
+using System.Globalization;
+using Foundation;
+using LightList.Data;
 using LightList.Services;
 using LightList.Utils;
+using Microsoft.Maui.Platform;
+using UIKit;
 
 namespace LightList;
 
@@ -12,6 +16,7 @@ public partial class App : Application
         Logger.Log("App initializing");
         InitializeComponent();
         _shell = shell;
+        CustomiseDatepicker();
         Logger.Log("App initialized");
     }
 
@@ -20,7 +25,27 @@ public partial class App : Application
         // TODO: remove / make this work properly
         if (Current != null)
             Current.UserAppTheme = AppTheme.Light;
+        
+        var culture = new CultureInfo("en-IE");
+        CultureInfo.DefaultThreadCurrentCulture = culture;
+        CultureInfo.DefaultThreadCurrentUICulture = culture;
 
         return new Window(_shell);
     }
+
+    private void CustomiseDatepicker()
+    {
+        Microsoft.Maui.Handlers.DatePickerHandler.Mapper.PrependToMapping(nameof(IDatePicker), (handler, view) =>
+        {
+#if IOS || MACCATALYST
+            if (handler.PlatformView.InputView is UIDatePicker picker)
+            {
+                picker.PreferredDatePickerStyle = UIDatePickerStyle.Inline;
+                picker.Mode = UIDatePickerMode.DateAndTime; // TODO: doesn't work. Why?
+                picker.Frame = new CoreGraphics.CGRect(0, 0, UIScreen.MainScreen.Bounds.Width, UIScreen.MainScreen.Bounds.Height);
+            }
+#endif
+        });
+    }
+    
 }
